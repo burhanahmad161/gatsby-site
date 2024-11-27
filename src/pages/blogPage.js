2dc04f68bf0cbf8c559aad6cfc2676185d2e05a6
burhanahmad161 <2022cs191@student.uet.edu.pk>
2024-11-27 09:38:21 -0800

    dsfa

--- src/pages/blogPage.js
+++ src/pages/blogPage.js
@@ -1,16 +1,9 @@
-import React from "react";
-import { graphql } from "gatsby"; // Import graphql explicitly
-
-const BlogPost = ({ data }) => {
-    const post = data.markdownRemark;
-
+export default function BlogPage() {
     return (
         <main>
-            <h1>{post.frontmatter.title}</h1>
-            <p>{post.frontmatter.date}</p>
-            <div dangerouslySetInnerHTML={{ __html: post.html }} />
+            <h1>My Gatsby website</h1>
+            <p>A simple site built with gatsby</p>
+            <p>Done</p>
         </main>
     );
-};
-
-export default BlogPost;
+}
