b4077596f6e852b4714425e98e0021d9aedec333
burhanahmad161 <2022cs191@student.uet.edu.pk>
2024-11-27 09:02:46 -0800

    Updates

---
+++ component---src-pages-blog-page-js-ef618fee7226b458fbbc.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[827],{1111:function(e,t,n){n.r(t),n.d(t,{default:function(){return l}});var a=n(7294);function l(){return a.createElement("main",null,a.createElement("h1",null,"Blog Page"),a.createElement("p",null,"This is the blog page"))}}}]);
+//# sourceMappingURL=component---src-pages-blog-page-js-ef618fee7226b458fbbc.js.map
